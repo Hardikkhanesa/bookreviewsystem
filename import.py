42d5269dba04b86b70a016ecd28e20bd3f867d55
vddesai1871 <vddesai1998@gmail.com>
2019-04-17 18:29:32 +0530

    Create books table and insert data from books.csv into it

--- import.py
+++ import.py
@@ -1,28 +1,74 @@
 import csv
 import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+import sqlite3
 
-#engine = create_engine(os.getenv("DATABASE_URL"))
-engine = create_engine('postgres://nysqtbkbaoexhh:04dccd0f6c64186ca6b9d49f288b49b85930ea913d560828c79aa5fe4fd1e256@ec2-54-221-201-212.compute-1.amazonaws.com:5432/d6v3stt6deu3s5');
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by the db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    try:
+        con = sqlite3.connect(db_file, check_same_thread=False)
+        return con
+    except sqlite3.Error as e:
+        print(e)
 
-db = scoped_session(sessionmaker(bind=engine))
+    return None
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except sqlite3.Error as e:
+        print(e)
 
 
 def main():
     f=open("books.csv")
-    reader = csv.reader(f);
-    for isbn,title,author,year in reader:
-        db.execute("INSERT INTO books (isbn,title,author,year) VALUES (:isbn,:title,:author,:year)",{"isbn":isbn,"title":title,"author":author,"year":year})
-        print(f"Added Books of {title} ");
-    db.commit()
+    conn = create_connection("books1.db")
+    sql_create_books_table = """ CREATE TABLE books (
+                                            isbn text,
+                                            title text,
+                                            author text,
+                                            year text
+                                        ); """
+
+    create_table(conn, sql_create_books_table)
+    reader = csv.reader(f)
+    i = 0
+    for isbn, title, author, year in reader:
+        i = i + 1
+        if i == 1:
+            continue
+        sql = ''' INSERT INTO books
+                      VALUES(?, ?, ?, ?) '''
+        cur = conn.cursor()
+        cur.execute(sql, (str(isbn), str(title), str(author), str(year)))
+
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM books")
+
+    rows = cur.fetchall()
+    print(rows[1])
+    print(len(rows))
+    conn.commit()
+    conn.close()
+
+
+
 
 if __name__=="__main__":
     main()
 
 #def main():
 #   f = open("flights.csv")
-#    reader = csv.reader(f)
+#   reader = csv.reader(f)
 #   for origin, destination, duration in reader:
 #        db.execute("INSERT INTO flights (origin, destination, duration) VALUES (:origin, :destination, :duration)",
 #                    {"origin": origin, "destination": destination, "duration": duration})
